8a0eae5ea4352d0de1f34ef246433f2b5502e9a1
depitcho <iamjosias5@gmail.com>
2025-07-15 17:35:36 +0100

    First Commit

---
+++ src/main/java/com/nerdtic/humanos/HumanOsApplication.java
@@ -0,0 +1,13 @@
+package com.nerdtic.humanos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanOsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HumanOsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nerdtic/humanos/HumanOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerdtic.humanos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanOsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
