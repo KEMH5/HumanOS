fe0e4cd246e279a8c5ca12997eb52b6097f37776
depitcho <iamjosias5@gmail.com>
2025-08-03 17:01:08 +0100

    Email Service done 80 percent

---
+++ src/main/java/com/nerdtic/humanos/auth/AuthenticationService.java
@@ -0,0 +1,106 @@
+package com.nerdtic.humanos.auth;
+
+
+import com.nerdtic.humanos.email.EmailService;
+import com.nerdtic.humanos.email.EmailTemplateName;
+import com.nerdtic.humanos.repositories.DepartementRepository;
+import com.nerdtic.humanos.repositories.FormationRepository;
+import com.nerdtic.humanos.security.role.RoleUtilisateurRepository;
+import com.nerdtic.humanos.security.user.Token;
+import com.nerdtic.humanos.security.user.TokenRepository;
+import com.nerdtic.humanos.security.user.User;
+import com.nerdtic.humanos.security.user.UserRepository;
+import jakarta.mail.MessagingException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    private RoleUtilisateurRepository roleUtilisateurRepository;
+    private DepartementRepository departementRepository;
+    private FormationRepository formationRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+    private final EmailService emailService;
+    @Value("${application.mailing.frontend.activation-url}")
+    private String activationUrl;
+
+
+
+    public void register(RegistrationRequest request) throws MessagingException {
+        var userRole = roleUtilisateurRepository.findById(
+                request.getRoleId()
+        ).orElseThrow(() -> new RuntimeException("ROLE USER was not initialized"));
+
+        var departement = departementRepository.findById(
+                request.getDepartementId()
+        ).orElseThrow(() -> new RuntimeException("USER Departement was not initialized"));
+
+        var formation = formationRepository.findById(
+                request.getFormationId()
+        ).orElseThrow(() -> new RuntimeException("Formation was not initialized"));
+
+        var user = User.builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .formations(List.of(formation))
+                .departement(departement)
+                .userRoles(List.of(userRole))
+                .build();
+        userRepository.save(user);
+        sendValidationEmail(user);
+
+    }
+
+    private void sendValidationEmail(User user) throws MessagingException {
+        var newToken = generateAndSendActivationToken(user);
+        emailService.sendEmail(
+                user.getEmail(),
+                user.fullName(),
+                EmailTemplateName.ACTIVATE_ACCOUNT,
+                activationUrl,
+                newToken,
+                "Account Activation"
+        );
+
+    }
+
+    private String generateAndSendActivationToken(User user) {
+        //generate Token
+        String generatedToken = generateActivationCode(4);
+        var token = Token.builder()
+                .Token(generatedToken)
+                .createdAt(LocalDateTime.now())
+                .expiredAt(LocalDateTime.now().plusMinutes(15))
+                .user(user)
+                .build();
+        tokenRepository.save(token);
+        return generatedToken;
+    }
+
+    private String generateActivationCode(int length) {
+        String characters = "0123456789";
+        StringBuilder codeBuilder = new StringBuilder();
+        SecureRandom secureRandom = new SecureRandom();
+        for (int i = 0; i < length; i++){
+            int randomIndex = secureRandom.nextInt(characters.length());
+            codeBuilder.append(characters.charAt(randomIndex));
+        }
+        return codeBuilder.toString();
+    }
+
+
+}
--- src/main/java/com/nerdtic/humanos/security/SecurityConfig.java
+++ src/main/java/com/nerdtic/humanos/security/SecurityConfig.java
@@ -1,13 +1,57 @@
 package com.nerdtic.humanos.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springdoc.webmvc.core.service.RequestService;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfig {
+
+    private JwtFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(reqr ->
+                        reqr.requestMatchers(
+                                "/auth/**",//First Line to permit request like login...
+                                "/v2/api-docs",
+                                "/v3/api-docs",
+                                "/v3/api-docs/**",
+                                "/swagger-ressources",
+                                "/swagger-ressources/**",
+                                "/configuration/ui",
+                                "/configuration/security",
+                                "/swagger-ui/**",
+                                "/webjars/**",
+                                "/swagger-ui.html"
+
+                        ).permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
+                .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
 }
